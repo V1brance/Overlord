704983a06dc057e5e2124e6ad2377279de915f8f
Neon Riot <sbrookshire99@gmail.com>
2021-09-15 00:55:17 -0500

    began adding functionality to the skills component

--- components/character/character.js
+++ components/character/character.js
@@ -2,14 +2,20 @@ import Stats from "./components/stats/stats";
 import Topbar from "./components/topbar/topbar";
 import Spells from "./components/spells/spells";
 import Weapons from "./components/weapons/weapons";
+import Skills from "./components/skills/skills";
+
+import styles from "./character.module.css";
 
 export default function Character() {
   return (
     <>
       <Topbar />
-      <Stats />
-      <Spells />
-      <Weapons />
+      <div className={styles.charInfo}>
+        <Stats />
+        <Skills />
+        <Spells />
+        <Weapons />
+      </div>
     </>
   );
 }
--- components/character/components/spells/spells.js
+++ components/character/components/spells/spells.js
@@ -1,9 +1,25 @@
 import styles from "./spells.module.css";
+import { useState } from "react";
 
 export default function Spells() {
+  const playerClass = "warlock";
+
+  const [lvlOne, setLvlOne] = useState(0);
+  const [lvlTwo, setLvlTwo] = useState(0);
+  const [lvlThree, setLvlThree] = useState(0);
+  const [lvlFour, setLvlFour] = useState(0);
+  const [lvlFive, setLvlFive] = useState(3);
+  const [lvlSix, setLvlSix] = useState(1);
+  const [lvlSeven, setLvlSeven] = useState(0);
+  const [lvlEight, setLvlEight] = useState(0);
+  const [lvlNine, setLvlNine] = useState(0);
+
   return (
-    <>
+    <div>
       <h1>Spells</h1>
-    </>
+      <div className={styles.slots}>
+        <div className={styles.levelArea}></div>
+      </div>
+    </div>
   );
 }
